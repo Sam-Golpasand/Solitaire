d69f33678414b404ff39c60d879a31c448899437
Sam Golpasand <samgolpasand45@gmail.com>
2026-04-16 15:25:28 +0200

    added input check

--- main.c
+++ main.c
@@ -1,23 +1,60 @@
 #include <stdio.h>
 #include "linkedList.h"
+#include <string.h>
 
-void stdOut(char *buffer);
+int notImplemented();
+int checkInput(char *command);
+void stdOut(char *command, int *lastIsValid);
 
 int main(void) {
 
-    char buffer[8] = {};
+    char command[8] = "";
+    int lastIsValid = 1;
 
     while(1) {
-        stdOut(buffer);
+        stdOut(command, &lastIsValid);
     }
 
     return 0;
 }
 
-void stdOut(char *buffer) {
-    printf("LAST Command: %s\n", buffer);
-    printf("Message: \n");
+void stdOut(char *command, int *lastIsValid) {
+
+
+    printf("LAST Command: %s\n", command);
+    printf("Message: ");
+    if (*lastIsValid == 0) {
+        printf("last command not valid \n");
+    } else {
+        printf("\n");
+    }
+
     printf("INPUT> ");
-    scanf("%s", buffer);
+    scanf("%s", command);
+    *lastIsValid = checkInput(command);
     printf("\n"); // make this print the SW when it is done.
+}
+
+int checkInput(char *command) {
+
+    // could change to a switch statement later with hashing and enums. To lazy to do now
+    if (strcmp(command, "LD") == 0) {
+        return notImplemented();
+    } else if (strcmp(command, "SW") == 0) {
+        return notImplemented();
+    } else if (strcmp(command, "SL") == 0) {
+        return notImplemented();
+    } else if (strcmp(command, "SR") == 0) {
+        return notImplemented();
+    } else if (strcmp(command, "SD") == 0) {
+        return notImplemented();
+    } else if (strcmp(command, "QQ") == 0) {
+        return notImplemented();
+    } else {
+        return 0;
+    }
+}
+
+int notImplemented() {
+    return 1;
 }

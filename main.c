6d6c47f499df78b1e1df0dacfc82281131b276c8
Sam Golpasand <samgolpasand45@gmail.com>
2026-04-16 15:26:15 +0200

    comments

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ void stdOut(char *command, int *lastIsValid);
 int main(void) {
 
     char command[8] = "";
-    int lastIsValid = 1;
+    int lastIsValid = 1; // 1 for valid, 0 for invalid
 
     while(1) {
         stdOut(command, &lastIsValid);
@@ -23,6 +23,7 @@ void stdOut(char *command, int *lastIsValid) {
 
     printf("LAST Command: %s\n", command);
     printf("Message: ");
+
     if (*lastIsValid == 0) {
         printf("last command not valid \n");
     } else {
@@ -38,6 +39,7 @@ void stdOut(char *command, int *lastIsValid) {
 int checkInput(char *command) {
 
     // could change to a switch statement later with hashing and enums. To lazy to do now
+    // Change the notImplemented() methods to your command function.
     if (strcmp(command, "LD") == 0) {
         return notImplemented();
     } else if (strcmp(command, "SW") == 0) {

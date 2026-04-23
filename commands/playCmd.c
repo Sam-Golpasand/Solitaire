99f37dd839f5084b555db130b122b4768edac9a2
safi2 <alsafi212@gmail.com>
2026-04-23 16:29:22 +0200

    Added logics

--- commands/playCmd.c
+++ commands/playCmd.c
@@ -2,6 +2,20 @@
 
 #include "../utils/linkedList.h"
 
+void appendNode(Node **head, Node *node) {
+    Node *tmp = *head;
+
+    if (*head == NULL) {
+        *head = node;
+        return;
+    }
+
+    while (tmp->next != NULL) {
+        tmp = tmp->next;
+    }
+    tmp->next = node;
+}
+
 void play(Node *head, Board *board) {
     int ROW = 0;
     Node *current = head;
@@ -10,47 +24,63 @@ void play(Node *head, Board *board) {
         return;
     }
 
-    while (current != NULL) {
-        if (ROW < 1) {
-            board->C1 = current;
-            current = current->next;
+    board->C1 = NULL;
+    board->C2 = NULL;
+    board->C3 = NULL;
+    board->C4 = NULL;
+    board->C5 = NULL;
+    board->C6 = NULL;
+    board->C7 = NULL;
+
+    while (current != NULL && ROW < 11) {
+        if (ROW <= 0 && current != NULL) {
+            Node *next = current->next;
+            current->next = NULL;
+            appendNode(&board->C1, current);
+            current = next;
         }
 
-        if (ROW < 6) {
-            board->C2 = current;
-            current = current->next;
+        if (ROW <= 5 && current != NULL) {
+            Node *next = current->next;
+            current->next = NULL;
+            appendNode(&board->C2, current);
+            current = next;
         }
 
-        if (ROW < 7) {
-            board->C3 = current;
-            current = current->next;
+        if (ROW <= 6 && current != NULL) {
+            Node *next = current->next;
+            current->next = NULL;
+            appendNode(&board->C3, current);
+            current = next;
         }
 
-        if (ROW < 8) {
-            board->C4 = current;
-            current = current->next;
+        if (ROW <= 7 && current != NULL) {
+            Node *next = current->next;
+            current->next = NULL;
+            appendNode(&board->C4, current);
+            current = next;
         }
 
-        if (ROW < 9) {
-            board->C5 = current;
-            current = current->next;
+        if (ROW <= 8 && current != NULL) {
+            Node *next = current->next;
+            current->next = NULL;
+            appendNode(&board->C5, current);
+            current = next;
         }
 
-        if (ROW < 10) {
-            board->C6 = current;
-            current = current->next;
+        if (ROW <= 9 && current != NULL) {
+            Node *next = current->next;
+            current->next = NULL;
+            appendNode(&board->C6, current);
+            current = next;
         }
 
-        if (ROW < 11) {
-            board->C7 = current;
-            current = current->next;
+        if (ROW <= 10 && current != NULL) {
+            Node *next = current->next;
+            current->next = NULL;
+            appendNode(&board->C7, current);
+            current = next;
         }
-
         ROW++;
     }
-}
-
-static void appendNode(Card card, Node *head) {
-
-
 }

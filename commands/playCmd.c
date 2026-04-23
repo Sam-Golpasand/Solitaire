83543cee9273586b0870b40ed45804297bb000f6
safi2 <alsafi212@gmail.com>
2026-04-23 14:42:32 +0200

    small changes

--- commands/playCmd.c
+++ commands/playCmd.c
@@ -2,5 +2,4 @@
 
 void play(Node *head, Board *board) {
 
-
 }
--- commands/playCmd.h
+++ commands/playCmd.h
@@ -2,6 +2,6 @@
 #define PLAYCMD_H
 #include "../utils/linkedList.h"
 
-void play(Node *head);
+void play(Node *head, Board *board);
 
 #endif //PLAYCMD_H

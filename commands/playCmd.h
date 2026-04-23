186f5d3d643be905133261c6a87929219d012208
safi2 <alsafi212@gmail.com>
2026-04-23 14:31:05 +0200

    Added pointers in struct

--- commands/playCmd.h
+++ commands/playCmd.h
@@ -2,11 +2,6 @@
 #define PLAYCMD_H
 #include "../utils/linkedList.h"
 
-typedef struct {
-    Node *rows[11];
-    Node *columns[7];
-} gameState;
-
 void play(Node *head);
 
 #endif //PLAYCMD_H
--- utils/utils.h
+++ utils/utils.h
@@ -6,20 +6,6 @@ typedef enum {
     STARTUP, PLAY
 } phase;
 
-typedef struct {
-    Node *C1;
-    Node *C2;
-    Node *C3;
-    Node *C4;
-    Node *C5;
-    Node *C6;
-    Node *C7;
-    Node *F1;
-    Node *F2;
-    Node *F3;
-    Node *F4;
-} board;
-
 int notImplemented();
 int commandHandler(char *command, Node **head, phase *currentPhase);
 

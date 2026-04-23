8b400cf1a43d494fd043171933fff9abc90dda10
safi2 <alsafi212@gmail.com>
2026-04-23 14:26:06 +0200

    Fixed comments and made the header file

--- utils/utils.h
+++ utils/utils.h
@@ -6,6 +6,20 @@ typedef enum {
     STARTUP, PLAY
 } phase;
 
+typedef struct {
+    Node *C1;
+    Node *C2;
+    Node *C3;
+    Node *C4;
+    Node *C5;
+    Node *C6;
+    Node *C7;
+    Node *C8;
+    Node *C9;
+    Node *C10;
+    Node *C11;
+} board;
+
 int notImplemented();
 int commandHandler(char *command, Node **head, phase *currentPhase);
 

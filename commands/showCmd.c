33e7d1375a230aee23eafbd7d626dd1b07ac4575
popping168 <albert.kirk168@gmail.com>
2026-05-07 14:51:17 +0200

    showBoard updated

--- commands/showCmd.c
+++ commands/showCmd.c
@@ -34,7 +34,7 @@ int show(Node *head, int visible) {
 
         //If 7 cards are printed go to next row, for every second row, make a field (idk the name)
         if (counter % 7 == 0) {
-            if (counter % 2 != 0) {
+            if (fieldNum < 5) {
                 printf("%s%s%s%c%i", tab, "[]", tab, 'F', fieldNum);
                 fieldNum++;
             }
@@ -119,11 +119,11 @@ int showBoard(Board *board) {
 void STARTUPShow() {
     char tab[] = "\t";
 
-    printf("C1%sC2%sC3%sC4%sC5%sC6%sC7\n", tab, tab, tab, tab, tab, tab, tab);
+    printf("C1%sC2%sC3%sC4%sC5%sC6%sC7\n", tab, tab, tab, tab, tab, tab);
 
     for (int i = 1; i <= 4; i++) {
-        printf("\n%s%s%s%s%s%s%s%s%s%s[]%sF%d\n",
-        tab, tab, tab, tab, tab, tab, tab, tab, tab, tab, tab, i);
+        printf("%s%s%s%s%s%s%s[]F%d\n",
+        tab, tab, tab, tab, tab, tab, tab, i);
     }
     printf("\n\n");
 }
